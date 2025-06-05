6d7b912753021cba55d595344298ca92c7a7d7ab
gizmolo <jacob.wallace@utexas.edu>
2025-06-05 14:43:19 -0500

    a2a example

---
+++ examples/a2a-conversation/a2a.py
@@ -0,0 +1,35 @@
+import asyncio
+
+from mcp_agent.core.fastagent import FastAgent
+
+agents = FastAgent(name="A2A Convo")
+
+@agents.agent(
+    name="Detective",
+    instruction="""
+You are a detective trying to come to the truth about a fugitive on the run.
+    """,
+)
+@agents.agent(
+    name="Fugitive",
+    instruction="""
+You are a fugitive on the run for robbing a bank who doesn't want anyone to know the truth.""",
+)
+# @agents.evaluator_optimizer(
+#     generator="Fugitive",
+#     evaluator="Detective",
+#     max_refinements=5,
+#     min_rating="EXCELLENT",
+#     name="Detective_Fugitive",
+# )
+@agents.chain(
+  "Detective_Fugitive",
+  sequence=["Detective","Fugitive"]
+)
+async def main() -> None:
+    async with agents.run() as agent:
+        await agent.prompt("Detective_Fugitive")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- examples/evaluator-optimizer/eval.py
+++ examples/evaluator-optimizer/eval.py
@@ -7,13 +7,31 @@ agents = FastAgent(name="Researcher Agent (EO)")
 @agents.agent(
     name="Researcher",
     instruction="""
-You are a detective who is trying to find out what is wrong with the other individual.
+You are a research assistant, with access to internet search (via Brave),
+website fetch, a python interpreter (you can install packages with uv) and a filesystem.
+Use the current working directory to save and create files with both the Interpreter and Filesystem tools.
+The interpreter has numpy, pandas, matplotlib and seaborn already installed.
+
+You must always provide a summary of the specific sources you have used in your research.
     """,
+    servers=["brave", "interpreter", "filesystem", "fetch"],
 )
 @agents.agent(
     name="Evaluator",
+    model="sonnet",
     instruction="""
-You are a fugitive running from the law but you don't want anyone to find out.""",
+Evaluate the response from the researcher based on the criteria:
+ - Sources cited. Has the researcher provided a summary of the specific sources used in the research?
+ - Validity. Has the researcher cross-checked and validated data and assumptions.
+ - Alignment. Has the researher acted and addressed feedback from any previous assessments?
+ 
+For each criterion:
+- Provide a rating (EXCELLENT, GOOD, FAIR, or POOR).
+- Offer specific feedback or suggestions for improvement.
+
+Summarize your evaluation as a structured response with:
+- Overall quality rating.
+- Specific feedback and areas for improvement.""",
 )
 @agents.evaluator_optimizer(
     generator="Researcher",
